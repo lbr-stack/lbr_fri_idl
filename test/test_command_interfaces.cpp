679959d5c0f9c5d1c69fafcfc4ec2c2ef7ba6ee3
mhubii <m.huber_1994@hotmail.de>
2024-05-10 15:32:12 +0100

    added shape tests

---
+++ test/test_command_interfaces.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+
+#include "friLBRState.h"
+
+#include "lbr_fri_msgs/msg/lbr_command.hpp"
+#include "lbr_fri_msgs/msg/lbr_position_command.hpp"
+#include "lbr_fri_msgs/msg/lbr_torque_command.hpp"
+#include "lbr_fri_msgs/msg/lbr_wrench_command.hpp"
+
+TEST(lbr_fri_msgs, test_command_size) {
+  using namespace KUKA::FRI;
+
+  lbr_fri_msgs::msg::LBRCommand lbr_command;
+  EXPECT_EQ(lbr_command.joint_position.size(), LBRState::NUMBER_OF_JOINTS);
+  EXPECT_EQ(lbr_command.torque.size(), LBRState::NUMBER_OF_JOINTS);
+
+  lbr_fri_msgs::msg::LBRPositionCommand lbr_position_command;
+  EXPECT_EQ(lbr_position_command.joint_position.size(), LBRState::NUMBER_OF_JOINTS);
+
+  lbr_fri_msgs::msg::LBRTorqueCommand lbr_torque_command;
+  EXPECT_EQ(lbr_torque_command.torque.size(), LBRState::NUMBER_OF_JOINTS);
+  EXPECT_EQ(lbr_torque_command.joint_position.size(), LBRState::NUMBER_OF_JOINTS);
+
+  lbr_fri_msgs::msg::LBRWrenchCommand lbr_wrench_command;
+  EXPECT_EQ(lbr_wrench_command.joint_position.size(), LBRState::NUMBER_OF_JOINTS);
+  EXPECT_EQ(lbr_wrench_command.wrench.size(), 6);
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
